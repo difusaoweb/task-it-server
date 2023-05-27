26dbbf5a11ff15552815e22850d2d5b8e9fcc384
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-05-27 20:42:52 -0300

    Tabela payment_types foi atualizada.

--- database/migrations/1591729878601_payment_types_schema.ts
+++ database/migrations/1591729878601_payment_types_schema.ts
@@ -5,7 +5,7 @@ export default class PaymentTypes extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id').primary().notNullable()
       table.string('title', 100).notNullable()
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).nullable()
