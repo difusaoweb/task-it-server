8e15c78f066028d6f2e1f625bd05b351d4409436
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-05-27 19:27:57 -0300

    Model User foi refatorado.

---
+++ app/Models/User.ts
@@ -0,0 +1,45 @@
+import { BaseModel, column, beforeSave } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { DateTime } from 'luxon'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public username: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public type: string
+
+  @column()
+  public validated: boolean
+
+  @column()
+  public token: DateTime
+
+  @column()
+  public isInvited: boolean
+
+  @column()
+  public asActiveInvite: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime | null
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
