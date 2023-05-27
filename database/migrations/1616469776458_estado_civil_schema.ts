180c642e17ad1ec1fbf65fd8aa7c2e7111253b11
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-05-27 20:29:09 -0300

    Tabela estado_civils foi atualizada.

--- database/migrations/1616469776458_estado_civil_schema.ts
+++ database/migrations/1616469776458_estado_civil_schema.ts
@@ -5,7 +5,7 @@ export default class EstadoCivil extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id').primary().notNullable()
       table.string('title', 2000).notNullable()
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).nullable()
