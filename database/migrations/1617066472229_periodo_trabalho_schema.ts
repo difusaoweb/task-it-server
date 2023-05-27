8918c8ac9405003ed18d9ae6500eb51faa3fc55e
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-05-27 20:53:07 -0300

    Tabela pesquisa_salarios foi atualizada.

--- database/migrations/1617066472229_periodo_trabalho_schema.ts
+++ database/migrations/1617066472229_periodo_trabalho_schema.ts
@@ -1,6 +1,6 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class Sexo extends BaseSchema {
+export default class PeriodoTrabalho extends BaseSchema {
   protected tableName = 'periodo_trabalhos'
 
   public async up() {
