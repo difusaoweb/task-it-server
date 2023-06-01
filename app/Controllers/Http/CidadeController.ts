fecbaf5fb6a8622a54bd2feefda0059c5ddf019c
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-06-01 15:14:59 -0300

    Controller Cidade foi atualizado.

--- app/Controllers/Http/CidadeController.ts
+++ app/Controllers/Http/CidadeController.ts
@@ -1,13 +1,25 @@
-'use strict'
-const Database = use('Database')
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
-class CidadeController {
-  async index ({ request }) {
-    const { title } = request.all()
+export default class CidadeController {
+  public async index({ auth, request, response }: HttpContextContract) {
+    const controllerSchema = schema.create({
+      title: schema.string()
+    })
+    try {
+      const { title } = await request.validate({ schema: controllerSchema })
 
-    const cidades = await Database.table('cidades').where('title', 'ILIKE', '%' + title + '%').limit(10)
-    return cidades
+      const cidades = await Database.from('cidades')
+        .where('title', 'ILIKE', '%' + title + '%')
+        .limit(10)
+
+      response.send(cidades)
+      return response
+    } catch (err) {
+      console.error(err)
+      response.status(500)
+      return response
+    }
   }
 }
-
-module.exports = CidadeController
