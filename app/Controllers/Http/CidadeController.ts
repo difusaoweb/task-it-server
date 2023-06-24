824be168f9b3599597091857d018df8606e83811
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-06-24 09:16:53 -0300

    /cities retorna todas as cidades, caso queria pesquisar, informe o title.

--- app/Controllers/Http/CidadeController.ts
+++ app/Controllers/Http/CidadeController.ts
@@ -1,20 +1,25 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { schema } from '@ioc:Adonis/Core/Validator'
+import Cidade from 'App/Models/Cidade'
 
 export default class CidadeController {
   public async index({ request, response }: HttpContextContract) {
     const controllerSchema = schema.create({
-      title: schema.string()
+      title: schema.string.nullableAndOptional()
     })
     try {
       const { title } = await request.validate({ schema: controllerSchema })
+      let returnDb: any = null
+      if (typeof title === 'string') {
+        returnDb = await Database.from('cidades')
+          .where('title', 'ILIKE', '%' + title + '%')
+          .limit(10)
+      } else {
+        returnDb = await Cidade.all()
+      }
 
-      const cidades = await Database.from('cidades')
-        .where('title', 'ILIKE', '%' + title + '%')
-        .limit(10)
-
-      response.send(cidades)
+      response.send(returnDb)
       return response
     } catch (err) {
       console.error(err)
