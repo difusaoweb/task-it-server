ddb7af90d4dada34ec3f5a0c9705815d0d22b812
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2023-07-19 11:20:26 -0300

    Controller Curriculum foi melhorado.

--- app/Controllers/Http/CurriculumController.ts
+++ app/Controllers/Http/CurriculumController.ts
@@ -61,57 +61,57 @@ export default class CurriculumController {
 
   public async show({ auth, request, response }: HttpContextContract) {
     const controllerSchema = schema.create({
-      curriculumId: schema.number.optional()
+      curriculumId: schema.number()
     })
     try {
       let { curriculumId } = await request.validate({
         schema: controllerSchema
       })
 
-      if (curriculumId === undefined) {
-        const user = auth.use('api').user
-        if (user === undefined) {
-          throw new Exception('', 403, 'TOKEN_USER_INVALID')
-        }
-
-        const professional = await Professional.findBy('user_id', user.id)
-        if (professional === null) {
-          throw new Exception('', 404, 'PROFESSIONAL_NOT_FOUND')
-        }
-
-        curriculumId = professional.id
-      }
-
-      const professional = await Database.from('professional')
+      const professional = await Database.from('professionals')
         .select(
           'professionals.*',
           'cities.title as cityTitle',
-          'educational_levels.title as educational_levels.title',
+          'educational_levels.title as educationalLevelTitle',
           'job_workloads.title as jobWorkloadTitle',
-          'desired_jobs.title_function as vagaDesejada',
-          'sexes.title as sexo',
-          'marital_statuses.title as stateCivil'
+          'desired_jobs.title_function as desiredJobTitleFunction',
+          'sexes.title as sexTitle',
+          'marital_statuses.title as maritalStatusTitle',
+          'employment_regimes.title as employmentRegimeTitle'
         )
-        .innerJoin('cities', 'professionals.city_id', 'cities.id')
-        .innerJoin('educational_levels', 'educational_levels.id', 'professionals.escolaridade_id')
-        .leftJoin('area_professional', 'professionals.area_atuacao_id', 'job_workloads.id')
-        .innerJoin('vaga_desejadas', 'professionals.vaga_desejada_id', 'vaga_desejadas.id')
-        .innerJoin('sexos', 'professionals.sexo_id', 'sexes.id')
-        .innerJoin('marital_statuses', 'professionals.marital_status_id', 'marital_statuses.id')
+        .innerJoin('cities', 'cities.id', 'professionals.city_id')
+        .innerJoin(
+          'educational_levels',
+          'educational_levels.id',
+          'professionals.educational_level_id'
+        )
+        .leftJoin('job_workloads', 'job_workloads.id', 'professionals.job_workload_id')
+        .innerJoin('desired_jobs', 'desired_jobs.id', 'professionals.desired_job_id')
+        .innerJoin('sexes', 'sexes.id', 'professionals.sex_id')
+        .innerJoin('marital_statuses', 'marital_statuses.id', 'professionals.marital_status_id')
+        .innerJoin(
+          'employment_regimes',
+          'employment_regimes.id',
+          'professionals.employment_regime_id'
+        )
         .where('professionals.id', curriculumId)
+      if (professional.length === 0) {
+        throw {
+          status: 404,
+          code: 'PROFESSIONAL_NOT_FOUND'
+        }
+      }
 
-      console.log(professional)
-
-      const skills = await Database.from('skills_professional as hp')
+      const skills = await Database.from('skill_professional as hp')
         .select('h.*')
         .innerJoin('skills as h', 'h.id', 'hp.skill_id')
         .where('hp.professional_id', curriculumId)
 
-      const experiences = await Database.from('experiences_of_professional as ep')
-        .select('ep.business', 'ep.start_date', 'ep.end_date', 'ep.current', 'ep.role')
+      const experiences = await Database.from('experiences_of_professionals as ep')
+        .select('ep.business', 'ep.start_date', 'ep.end_date', 'ep.role')
         .where('ep.professional_id', curriculumId)
 
-      const courses = await Database.from('courses_of_professional as cs')
+      const courses = await Database.from('courses_of_professionals as cs')
         .select('cs.institution', 'cs.start_date', 'cs.end_date', 'cs.course')
         .where('cs.professional_id', curriculumId)
 
@@ -122,20 +122,104 @@ export default class CurriculumController {
         courses
       })
     } catch (err: any) {
-      // console.error(err)
       let status = 500
       let failure: any = { code: 'UNKNOWN' }
+      if (err.status) {
+        status = err.status
+      }
       if (err.code) {
         failure.code = err.code
       }
+
       switch (failure.code) {
+        case 'PROFESSIONAL_NOT_FOUND':
+          break
         case 'UNKNOWN':
+          console.error(new Date(), 'app/Controllers/Http/CurriculumController.ts show')
           console.error(err)
           break
       }
       return response.status(status).send(failure)
     }
   }
+
+  // public async show({ auth, request, response }: HttpContextContract) {
+  //   const controllerSchema = schema.create({
+  //     curriculumId: schema.number.optional()
+  //   })
+  //   try {
+  //     let { curriculumId } = await request.validate({
+  //       schema: controllerSchema
+  //     })
+
+  //     if (curriculumId === undefined) {
+  //       const user = auth.use('api').user
+  //       if (user === undefined) {
+  //         throw new Exception('', 403, 'TOKEN_USER_INVALID')
+  //       }
+
+  //       const professional = await Professional.findBy('user_id', user.id)
+  //       if (professional === null) {
+  //         throw new Exception('', 404, 'PROFESSIONAL_NOT_FOUND')
+  //       }
+
+  //       curriculumId = professional.id
+  //     }
+
+  //     const professional = await Database.from('professional')
+  //       .select(
+  //         'professionals.*',
+  //         'cities.title as cityTitle',
+  //         'educational_levels.title as educational_levels.title',
+  //         'job_workloads.title as jobWorkloadTitle',
+  //         'desired_jobs.title_function as vagaDesejada',
+  //         'sexes.title as sexo',
+  //         'marital_statuses.title as stateCivil'
+  //       )
+  //       .innerJoin('cities', 'professionals.city_id', 'cities.id')
+  //       .innerJoin('educational_levels', 'educational_levels.id', 'professionals.escolaridade_id')
+  //       .leftJoin('area_professional', 'professionals.area_atuacao_id', 'job_workloads.id')
+  //       .innerJoin('vaga_desejadas', 'professionals.vaga_desejada_id', 'vaga_desejadas.id')
+  //       .innerJoin('sexos', 'professionals.sexo_id', 'sexes.id')
+  //       .innerJoin('marital_statuses', 'professionals.marital_status_id', 'marital_statuses.id')
+  //       .where('professionals.id', curriculumId)
+
+  //     console.log(professional)
+
+  //     const skills = await Database.from('skills_professional as hp')
+  //       .select('h.*')
+  //       .innerJoin('skills as h', 'h.id', 'hp.skill_id')
+  //       .where('hp.professional_id', curriculumId)
+
+  //     const experiences = await Database.from('experiences_of_professional as ep')
+  //       .select('ep.business', 'ep.start_date', 'ep.end_date', 'ep.current', 'ep.role')
+  //       .where('ep.professional_id', curriculumId)
+
+  //     const courses = await Database.from('courses_of_professional as cs')
+  //       .select('cs.institution', 'cs.start_date', 'cs.end_date', 'cs.course')
+  //       .where('cs.professional_id', curriculumId)
+
+  //     return response.send({
+  //       professional: professional.length === 0 ? {} : professional[0],
+  //       skills,
+  //       experiences,
+  //       courses
+  //     })
+  //   } catch (err: any) {
+  //     // console.error(err)
+  //     let status = 500
+  //     let failure: any = { code: 'UNKNOWN' }
+  //     if (err.code) {
+  //       failure.code = err.code
+  //     }
+  //     switch (failure.code) {
+  //       case 'UNKNOWN':
+  //         console.error(err)
+  //         break
+  //     }
+  //     return response.status(status).send(failure)
+  //   }
+  // }
 
   public async showDashboard({ auth, response }: HttpContextContract) {
     try {
